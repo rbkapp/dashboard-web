19e2a0acdba32b11f63cd80479bb702af0bd95db
nillgoterra <nillgoterra@gmail.com>
2022-02-22 13:38:59 -0400

    hotfix: test 03

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,14 @@ import {
 } from 'react-router-dom';
 
 import Index from "../pages/Login";
+import Request from "../pages/Request";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Index} />
+        <Route exact path="/request" component={Request} />
       </Switch>
     </Router>
   );
