716030221d942f895aa6892df58dc96ac7f8cee5
nillgoterra <nillgoterra@gmail.com>
2022-02-22 13:35:22 -0400

    hotfix: test 01

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,22 +5,12 @@ import {
 } from 'react-router-dom';
 
 import Index from "../pages/Login";
-import Request from "../pages/Request";
-import Budgets from "../pages/Budgets";
-import BudgetsDetails from "../pages/Budgets/Details";
-import Users from "../pages/Users";
-import Settings from "../pages/Settings";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Index} />
-        <Route exact path="/request" component={Request} />
-        <Route exact path="/budgets" component={Budgets} />
-        <Route exact path="/budgets/details/:id" component={BudgetsDetails} />
-        <Route exact path="/users" component={Users} />
-        <Route exact path="/settings" component={Settings} />
       </Switch>
     </Router>
   );
